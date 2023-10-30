4dd875e82351e62ec0ba91ba9545ba69f8280f52
Alexandre Correia <correiaalexandrealex@gmail.com>
2023-10-30 02:54:40 +0000

    refactoring game and logic files

--- src/play.pl
+++ src/play.pl
@@ -4,8 +4,9 @@
 :- consult('menu.pl').
 :- consult('utils.pl').
 :- consult('inputs.pl').
+:- consult('logic.pl').
 :- consult('game.pl').
 
 % Main function
 play :-
-    game_start(5, GameState).
+    game_start(3, GameState).
